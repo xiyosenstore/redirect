8389d426390c0dfd51765722ab23f5eff61e6e83
xiyosenstore <ziyosenstore@gmail.com>
2026-02-18 02:55:42 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'redirect31.benxx.workers.dev'
+      url.hostname = 'vles-8lh.pages.dev'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
